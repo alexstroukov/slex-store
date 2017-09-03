3a129fa91b0011a55a7dd7f5a3e34076b7699703
Slex Atroukov <slexatrukov@Alexs-MacBook-Pro-2.local>
2017-09-03 12:04:59 +0100

    updated dependencies

--- webpack/src.webpack.config.js
+++ webpack/src.webpack.config.js
@@ -37,7 +37,8 @@ const config = {
           presets: ['es2015'],
           plugins: [
             'transform-object-rest-spread',
-            'transform-es2015-destructuring'
+            'transform-es2015-destructuring',
+            'transform-class-properties'
           ]
         }
       },
